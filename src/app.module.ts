c6942e795d90b3d4a6eea68d50391ff6b6a44eb2
yangzhou <2638497222@qq.com>
2024-10-24 21:53:36 +0800

    system

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { SystemModule } from './modules/system/system.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, SystemModule],
   controllers: [AppController],
   providers: [AppService],
 })
