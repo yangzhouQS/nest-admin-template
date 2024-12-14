24a841fa3c56a2dafecdc2ac6161d980b55d4461
yangzhou <2638497222@qq.com>
2024-12-14 23:51:48 +0800

    fix: 删除多余auth接口

--- src/modules/system/auth/auth.controller.ts
+++ src/modules/system/auth/auth.controller.ts
@@ -1,15 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { CreateAuthDto } from "./dto/create-auth.dto";
-import { UpdateAuthDto } from "./dto/update-auth.dto";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { RegisterDto } from "./dto/register.dto";
 import { LoginDto } from "./dto/login.dto";
@@ -28,30 +18,5 @@ export class AuthController {
   @Post("/login")
   login(@Body() body: LoginDto) {
     return this.authService.login(body);
-  }
-
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.authService.remove(+id);
   }
 }
--- src/modules/system/auth/auth.service.ts
+++ src/modules/system/auth/auth.service.ts
@@ -1,6 +1,4 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
-import { CreateAuthDto } from "./dto/create-auth.dto";
-import { UpdateAuthDto } from "./dto/update-auth.dto";
 import { RegisterDto } from "./dto/register.dto";
 import { PrismaService } from "../../../shared/prisma/prisma.service";
 import { JwtService } from "@nestjs/jwt";
@@ -13,26 +11,6 @@ export class AuthService {
     private readonly prisma: PrismaService,
     private readonly jwt: JwtService,
   ) {}
-
-  create(createAuthDto: CreateAuthDto) {
-    return "This action adds a new auth";
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 
   public async register(body: RegisterDto) {
     const user = await this.prisma.user.create({
