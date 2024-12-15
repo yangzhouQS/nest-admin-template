61e730a85553b859dedf744d74155abab87e3ecc
yangzhou <2638497222@qq.com>
2024-12-15 20:49:23 +0800

    fix: format日志格式化

--- src/common/winston.config.ts
+++ src/common/winston.config.ts
@@ -1,48 +1,53 @@
-import * as chalk from 'chalk'; // 用于颜色化输出
+import * as chalk from "chalk"; // 用于颜色化输出
 // const chalk = require('chalk');
-import { createLogger, format, transports } from 'winston';
-import * as DailyRotateFile from 'winston-daily-rotate-file';
+import { createLogger, format, transports } from "winston";
+import * as DailyRotateFile from "winston-daily-rotate-file";
 
 // 定义日志级别颜色
 const levelsColors = {
-  error: 'red',
-  warn: 'yellow',
-  info: 'green',
-  debug: 'blue',
-  verbose: 'cyan',
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  debug: "blue",
+  verbose: "cyan",
 };
 
 const winstonLogger = createLogger({
   format: format.combine(
-    format.timestamp(),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf((info) => {
+      return `${chalk.yellow(info.timestamp)} ${chalk[levelsColors[info.level]](
+        info.level,
+      )}: ${info.message}`;
+    }),
     format.errors({ stack: true }),
     format.splat(),
     format.json(),
   ),
-  defaultMeta: { service: 'log-service' },
+  defaultMeta: { service: "log-service" },
   transports: [
     new DailyRotateFile({
-      filename: 'logs/errors/error-%DATE%.log', // 日志名称，占位符 %DATE% 取值为 datePattern 值。
-      datePattern: 'YYYY-MM-DD', // 日志轮换的频率，此处表示每天。
+      filename: "logs/errors/error-%DATE%.log", // 日志名称，占位符 %DATE% 取值为 datePattern 值。
+      datePattern: "YYYY-MM-DD", // 日志轮换的频率，此处表示每天。
       zippedArchive: true, // 是否通过压缩的方式归档被轮换的日志文件。
-      maxSize: '20m', // 设置日志文件的最大大小，m 表示 mb 。
-      maxFiles: '14d', // 保留日志文件的最大天数，此处表示自动删除超过 14 天的日志文件。
-      level: 'error', // 日志类型，此处表示只记录错误日志。
+      maxSize: "20m", // 设置日志文件的最大大小，m 表示 mb 。
+      maxFiles: "14d", // 保留日志文件的最大天数，此处表示自动删除超过 14 天的日志文件。
+      level: "error", // 日志类型，此处表示只记录错误日志。
     }),
     new DailyRotateFile({
-      filename: 'logs/warnings/warning-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
+      filename: "logs/warnings/warning-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-      level: 'warn',
+      maxSize: "20m",
+      maxFiles: "14d",
+      level: "warn",
     }),
     new DailyRotateFile({
-      filename: 'logs/app/app-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
+      filename: "logs/app/app-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
       zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
+      maxSize: "20m",
+      maxFiles: "14d",
     }),
     /*new transports.Console({
       format: format.combine(
